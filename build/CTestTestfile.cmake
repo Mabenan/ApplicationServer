27efd0e2408ce868e2cf86c655fbb698347f82ed
Daniel Richter <doener123@googlemail.com>
2019-11-04 08:36:46 +0100

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/drichte/Documents/ApplicationServer
-# Build directory: C:/Users/drichte/Documents/ApplicationServer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
